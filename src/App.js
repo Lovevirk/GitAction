0ef77fdae3be7d480ce35152193c77860ff584c5
Lovevirk <133934873+Lovevirk@users.noreply.github.com>
2025-02-25 16:23:21 +1300

    hi

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>   Ramandeep Singh</p>
+        <p>   Ramandeep Singh Australia</p>
         <a
           className="App-link"
           href="https://reactjs.org"
